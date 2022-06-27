2ffe43a980f92b5332c34913c9d358eb48f2fc29
goodtonoh <goodtonoh@gmail.com>
2022-06-27 15:35:05 +0900

    user 추가

---
+++ finalProject6/src/main/java/com/sparta/finalproject6/model/Post.java
@@ -0,0 +1,41 @@
+package com.sparta.finalproject6.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@SequenceGenerator(name = "POST_A",
+        sequenceName = "POST_B",
+        initialValue = 1, allocationSize = 100)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "POST_A")
+    @Column(name = "POST_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String imgUrl;
+
+    @Column(nullable = false)
+    private String content;
+
+//    @Column(nullable = false)
+//    private RegionCategory regionCategory;
+//
+//    @Column(nullable = false)
+//    private PriceCategory priceCategory;
+
+    @ManyToOne
+    private User user;
+
+}
--- finalProject6/src/main/java/com/sparta/finalproject6/model/User.java
+++ finalProject6/src/main/java/com/sparta/finalproject6/model/User.java
@@ -1,7 +1,48 @@
 package com.sparta.finalproject6.model;
 
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SequenceGenerator(
+        name = "USER_A",
+        sequenceName = "USER_B",
+        initialValue = 1, allocationSize = 50)
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_A")
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String userImgUrl;
+
+    @OneToMany
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String username, String nickname, String password) {
+        this.username = username;
+        this.nickname = nickname;
+        this.password = password;
+    }
 
 }
