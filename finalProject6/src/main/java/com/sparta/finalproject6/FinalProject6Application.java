5628e741ef389657271c90f505f586e2ba42a100
goodtonoh <goodtonoh@gmail.com>
2022-06-27 15:15:29 +0900

    initial commit

---
+++ finalProject6/src/main/java/com/sparta/finalproject6/FinalProject6Application.java
@@ -0,0 +1,13 @@
+package com.sparta.finalproject6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProject6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProject6Application.class, args);
+    }
+
+}
---
+++ finalProject6/src/test/java/com/sparta/finalproject6/FinalProject6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.finalproject6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProject6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
