e78b65e8088483efe019402f18faddc54ba15dec
printf-jin <dlghwls7662@naver.com>
2022-06-30 14:07:47 +0900

    CORS 설정 추가

--- finalProject6/src/main/java/com/sparta/finalproject6/config/CorsConfiguration.java
+++ finalProject6/src/main/java/com/sparta/finalproject6/config/CorsConfiguration.java
@@ -5,12 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET","POST","DELETE","PUT")
+        registry.addMapping("/*")
+                // .allowedOrigins("*")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .exposedHeaders("*")
                 .allowCredentials(true);
     }
