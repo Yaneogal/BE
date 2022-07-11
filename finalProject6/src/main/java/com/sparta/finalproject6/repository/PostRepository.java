5c117abb8c0e977828fe67db36add366518e5a3d
printf-jin <dlghwls7662@naver.com>
2022-07-11 16:50:17 +0900

    refactor: querydsl 추가로 인한 기존 코드 주석 처리
    주석 코드 내용: 기존 페이지 조회 메서드, 키워드 검색 메서드

--- finalProject6/src/main/java/com/sparta/finalproject6/repository/PostRepository.java
+++ finalProject6/src/main/java/com/sparta/finalproject6/repository/PostRepository.java
@@ -8,23 +8,30 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
-    @Query("select p from Post p")
-    Page<Post> findAllPosts(Pageable pageable);
+//    @Query("select p from Post p")
+//    Page<Post> findAllPosts(Pageable pageable);
+//
+//    @Query("select p from Post p")
+//    List<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
-    @Query("select p from Post p")
-    List<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
+//    @Query("select p from Post p left join fetch ThemeCategory t on p.id = t.post.id where p.title " +
+//            "like %:keyword% or p.content like %:keyword% " +
+//            "or p.regionCategory like %:keyword% " +
+//            "or p.priceCategory like %:keyword% " +
+//            "or t.themeCategory like %:keyword%")
+//    List<Post> findSearchKeyword(String keyword, Pageable pageable);
 
     Optional<Post> findById(Long postId);
 
     @Modifying
     @Query("update Post p set p.view = p.view + 1 where p.id = :id ")
     int updateView(Long id);
-
 
 }
