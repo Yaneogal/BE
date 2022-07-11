2c117efca76dd7f0864817411bee4bbe6083c731
printf-jin <dlghwls7662@naver.com>
2022-07-11 16:44:04 +0900

    refactor: PostResponseDto 수정
    querydsl 사용에 따른 조회 방식 변경으로 dto에 새로운 생성자 추가
    @QueryProjection 어노테이션 추가

--- finalProject6/src/main/java/com/sparta/finalproject6/dto/responseDto/PostResponseDto.java
+++ finalProject6/src/main/java/com/sparta/finalproject6/dto/responseDto/PostResponseDto.java
@@ -1,5 +1,6 @@
 package com.sparta.finalproject6.dto.responseDto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import com.sparta.finalproject6.dto.requestDto.ThemeCategoryDto;
 import lombok.Builder;
 import lombok.Data;
@@ -8,12 +9,13 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Getter
 @Builder
 @Data
 public class PostResponseDto {
 
     private Long postId;
+    private String nickName;
+    private String userImgUrl;
     private String title;
     private List<String> imgUrl;
     private String content;
@@ -31,4 +33,40 @@ public class PostResponseDto {
     private List<PostCommentResponseDto> comments;
 //    private List<LoveResponseDto> loves;
 
+
+    public PostResponseDto(Long postId, String nickName, String userImgUrl, String title, List<String> imgUrl, String content, int viewCount, int loveCount, int bookmarkCount, int commentCount, String regionCategory, String priceCategory, Boolean loveStatus, Boolean bookmarkStatus, List<ThemeCategoryDto> themeCategory, LocalDateTime createdAt, LocalDateTime modifiedAt, List<PostCommentResponseDto> comments) {
+        this.postId = postId;
+        this.nickName = nickName;
+        this.userImgUrl = userImgUrl;
+        this.title = title;
+        this.imgUrl = imgUrl;
+        this.content = content;
+        this.viewCount = viewCount;
+        this.loveCount = loveCount;
+        this.bookmarkCount = bookmarkCount;
+        this.commentCount = commentCount;
+        this.regionCategory = regionCategory;
+        this.priceCategory = priceCategory;
+        this.loveStatus = loveStatus;
+        this.bookmarkStatus = bookmarkStatus;
+        this.themeCategory = themeCategory;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.comments = comments;
+    }
+
+    @QueryProjection
+    public PostResponseDto(Long postId, String nickName, String userImgUrl, String title, String content, int bookmarkCount, int loveCount, String regionCategory, String priceCategory, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.postId = postId;
+        this.nickName = nickName;
+        this.userImgUrl = userImgUrl;
+        this.title = title;
+        this.content = content;
+        this.bookmarkCount = bookmarkCount;
+        this.loveCount = loveCount;
+        this.regionCategory = regionCategory;
+        this.priceCategory = priceCategory;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
 }
