16159035cd4bffd3516d08302220f11f32db3734
printf-jin <dlghwls7662@naver.com>
2022-07-21 17:48:40 +0900

    refactor: ControllerExceptionHandler 리팩토링 1. 반환 타입 ErrorResponse통일 2. HttpStatus @ResponseStatus 사용해 고정

--- finalProject6/src/main/java/com/sparta/finalproject6/handler/ControllerExceptionHandler.java
+++ finalProject6/src/main/java/com/sparta/finalproject6/handler/ControllerExceptionHandler.java
@@ -1,34 +1,79 @@
 package com.sparta.finalproject6.handler;
 
-import com.sparta.finalproject6.dto.responseDto.CMResponseDto;
-import com.sparta.finalproject6.handler.exception.CustomDuplicateException;
-import com.sparta.finalproject6.handler.exception.CustomSignUpValidException;
+import com.sparta.finalproject6.dto.responseDto.ErrorResponse;
+import com.sparta.finalproject6.handler.exception.SignUpValidException;
+import com.sparta.finalproject6.handler.exception.UserDuplicateException;
+import com.sparta.finalproject6.handler.exception.UserIdNotFoundException;
+import com.sparta.finalproject6.handler.exception.WrongPasswordException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(CustomSignUpValidException.class)
-    public ResponseEntity<CMResponseDto> validationException(CustomSignUpValidException e) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResponse validaionException(
+            MethodArgumentNotValidException e, HttpServletRequest request) {
+
         e.printStackTrace();
-        return new ResponseEntity<>(new CMResponseDto(e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
+        String path = request.getRequestURI();
+        return ErrorResponse.of(path, e.getBindingResult());
     }
 
-    @ExceptionHandler(CustomDuplicateException.class)
-    public ResponseEntity<CMResponseDto> duplicateException(CustomDuplicateException e) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResponse UserIdNotFoundException(
+            UserIdNotFoundException e, HttpServletRequest request) {
+
         e.printStackTrace();
-        return new ResponseEntity<>(new CMResponseDto(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        String path = request.getRequestURI();
+        return new ErrorResponse(e.getMessage(), path);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<CMResponseDto> nonExpectedException(Exception e) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResponse SignUpValidException(
+            SignUpValidException e, HttpServletRequest request) {
+
         e.printStackTrace();
-        return new ResponseEntity<>(new CMResponseDto("예상치 못한 문제가 발생되었습니다."), HttpStatus.INTERNAL_SERVER_ERROR);
+        String path = request.getRequestURI();
+        return new ErrorResponse(e.getMessage(), path);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ErrorResponse WrongPasswordException(
+            WrongPasswordException e, HttpServletRequest request) {
+
+        e.printStackTrace();
+        String path = request.getRequestURI();
+        return new ErrorResponse(e.getMessage(), path);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResponse UserDuplicateException(
+            UserDuplicateException e, HttpServletRequest request) {
+
+        e.printStackTrace();
+        String path = request.getRequestURI();
+        return new ErrorResponse(e.getMessage(), path);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResponse nonExpectedException(Exception e) {
+
+        e.printStackTrace();
+        return new ErrorResponse("예상치 못한 문제가 발생되었습니다.");
     }
 
 }
