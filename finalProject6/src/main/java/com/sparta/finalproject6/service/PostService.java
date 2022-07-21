5bf2cf8282fb7ebda3422b1165bde0d1940c596c
Mohorang <dlwnsgh253@gmail.com>
2022-07-21 22:25:45 +0900

    fix : 게시글 수정 이미지 indexOutOfBoundsException 해결

--- finalProject6/src/main/java/com/sparta/finalproject6/service/PostService.java
+++ finalProject6/src/main/java/com/sparta/finalproject6/service/PostService.java
@@ -383,10 +383,9 @@ public class PostService {
                 }
             }
 
-            List<String> deleteImgUrl = checkImgUrl(placeRequestDto.get(i),places.get(i));
-
             //장소 수 가 아직 기존장소 수 보다 작을때
             if(i < (places.size())) {
+                List<String> deleteImgUrl = checkImgUrl(placeRequestDto.get(i),places.get(i));
                 if(!multipartFile.isEmpty()){
                     imgResult = getImageList(files);
                     List<String> imgUrls = new ArrayList<>(imgResult.size());
