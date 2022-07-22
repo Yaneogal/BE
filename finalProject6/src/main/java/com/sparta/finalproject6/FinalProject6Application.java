078068aa88609253da334055cdc86bb2e341b889
printf-jin <dlghwls7662@naver.com>
2022-07-22 22:15:22 +0900

    test: cicd 배포 테스트

--- finalProject6/src/main/java/com/sparta/finalproject6/FinalProject6Application.java
+++ finalProject6/src/main/java/com/sparta/finalproject6/FinalProject6Application.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class FinalProject6Application {
     public static void main(String[] args) {
         SpringApplication.run(FinalProject6Application.class, args);
+        
     }
 
 }
