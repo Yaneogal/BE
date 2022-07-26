74e1ed41036a64cb3ba1ba658f357be2cedc9023
printf-jin <dlghwls7662@naver.com>
2022-07-26 11:31:36 +0900

    fix: deploy.sh 수정

--- finalProject6/deploy.sh
+++ finalProject6/deploy.sh
@@ -17,4 +17,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/ubuntu/sparta/build/libs/finalProject6-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar /home/ubuntu/sparta/build/libs/finalProject6-0.0.1-SNAPSHOT.jar &
