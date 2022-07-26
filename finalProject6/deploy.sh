4e2311fd6852f82a79aeeb117b13edcf52206c5b
printf-jin <dlghwls7662@naver.com>
2022-07-26 10:58:58 +0900

    fix: deploy.sh 경로 변경

--- finalProject6/deploy.sh
+++ finalProject6/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/sparta/*.jar)
+BUILD_JAR=$(ls /home/ubuntu/sparta/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/sparta/deploy.log
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/sparta/build/libs/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/sparta/deploy.log
+echo "> build 파일 복사" >> /home/ubuntu/sparta/build/libs/deploy.log
 DEPLOY_PATH=/home/ubuntu/sparta/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/sparta/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/sparta/build/libs/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/sparta/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/sparta/build/libs/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -20,5 +20,5 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/sparta/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/sparta/deploy.log 2>/home/ubuntu/sparta/deploy_err.log &
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/sparta/build/libs/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/sparta/build/libs/deploy.log 2>/home/ubuntu/sparta/build/libs/deploy_err.log &
