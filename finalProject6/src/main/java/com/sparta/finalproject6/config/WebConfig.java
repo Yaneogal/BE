2fe2a118c41264a69b7acb47b9cebe20eefcb284
printf-jin <dlghwls7662@naver.com>
2022-07-27 21:39:14 +0900

    no message

--- finalProject6/src/main/java/com/sparta/finalproject6/config/WebConfig.java
+++ finalProject6/src/main/java/com/sparta/finalproject6/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
                 .allowedOrigins("http://yaneogal.site")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET","POST","DELETE","PUT")
                 .exposedHeaders("*")
                 .allowCredentials(true);
